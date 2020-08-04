a5e1da031958330a3eaea865e5936efa35b236b1
松井 光之 <m.mitsuyuki1063@MacBook-Pro-2.local>
2020-08-04 18:30:50 +0900

    basics

---
+++ src/basics/type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 22
+	fmt.Printf("v is of type %T\n", v)
+	x := 3.14
+	fmt.Printf("x is of type %T\n", x)
+	r := 0.3 + 4.1i
+	fmt.Printf("r is of type %T\n", r)
+	s := "sss"
+	fmt.Printf("s is of type %T\n", s)
+}
